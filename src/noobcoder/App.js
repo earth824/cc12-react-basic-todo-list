613940f7b81da3dd4ca76af2089681e92f854f3a
pisutthee <pisut.flyinggiraffe@gmail.com>
2022-08-11 16:35:05 +0700

    complete ui

--- src/noobcoder/App.js
+++ src/noobcoder/App.js
@@ -1,6 +1,10 @@
 import EditBar from './components/EditBar';
+import Pagination from './components/Pagination';
 import SearchBox from './components/SearchBox';
 import SearchStatus from './components/SearchStatus';
+import ShowAmount from './components/ShowAmount';
+import ShowItem from './components/ShowItem';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
@@ -9,6 +13,12 @@ function App() {
       <div className="d-flex gap-3 my-4">
         <SearchBox />
         <SearchStatus />
+      </div>
+      <ShowItem />
+      <TodoList />
+      <div className="d-flex justify-content-between align-items-center">
+        <ShowAmount />
+        <Pagination />
       </div>
     </div>
   );
